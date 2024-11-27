0689816b78251ba6966b488a4742fd0d8e876710
KhanyiCodes <rebaone02khanyi@gmail.com>
2024-11-27 15:24:05 +0200

    saving and checking account initialisation

--- CC-BANK/src/main/java/com/code_compilers/CC_BANK/model/SavingsAccount.java
+++ CC-BANK/src/main/java/com/code_compilers/CC_BANK/model/SavingsAccount.java
@@ -1,4 +1,26 @@
 package com.code_compilers.CC_BANK.model;
 
-public class SavingsAccount {
+import jakarta.persistence.Entity;
+
+@Entity
+public class SavingsAccount extends Account {
+    private double interestRate;
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public String toString() {
+        return "SavingsAccount{" +
+                "id=" + getId() +
+                ", owner='" + getOwner() + '\'' +
+                ", balance=" + getBalance() +
+                ", interestRate=" + interestRate +
+                '}';
+    }
 }
